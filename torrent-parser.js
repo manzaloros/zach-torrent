93d2a11102e210ea4f00f5b40f6a78f8115d5098
manzaloros <zacharymansellcello@gmail.com>
2021-02-02 21:13:28 -0600

    Fill out getSize and infoHash

--- torrent-parser.js
+++ torrent-parser.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
 const bencode = require('bencode');
+const bignum = require('bignum');
 
 const open = async (filepath) => {
   const file = await fs.readFile(filepath);
   return bencode.decode(file);
 };
 
-const size = (torrent) => {};
+const getSize = (torrent) => {
+  const size = torrent.info.files
+    ? torrent.info.files.map((file) => file.length).reduce((a, b) => a + b)
+    : torrent.info.length;
 
-const infoHash = (torrent) => {};
+  return bignum.toBuffer(size, { size: 8 });
+};
+
+const infoHash = (torrent) => {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash('sha1').update(info).digest();
+};
 
 module.exports = { open, size, infoHash };
